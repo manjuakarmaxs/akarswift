60ca327ae2b22b824fd116afc0c8ef58a7d8928a
Manjunath <manjunath@akarmaxs.in>
2024-09-02 17:20:17 +0530

    final Update

--- src/component/About.js
+++ src/component/About.js
@@ -23,45 +23,46 @@ const About = () => {
   }, []);
 
   return (
-    <div className="container my-5" >
-      <div className="row">
-        <div className="col-md-4">
-          <div className="card hover-card" data-aos="fade-down">
-            <img src={vision} className="card-img" alt="Our Vision" />
-            <div className="card-img-overlay d-flex flex-column justify-content-center text-center">
-              <h2 className="card-title">Vision</h2>
-              <p className="card-text">
-                To digitalize every industry left and enhance them to compete with the elite. To leave an imprint in this digital world with quality services, client testimonials, and customer satisfaction. Finally to envision ourselves as a key partner in the upcoming digital revolution for all industries and businesses.
-              </p>
-            </div>
+    <div className="container my-5">
+    <div className="row">
+      <div className="col-lg-4 col-md-6">
+        <div className="card hover-card" data-aos="fade-down">
+          <img src={vision} className="card-img" alt="Our Vision" />
+          <div className="card-img-overlay d-flex flex-column justify-content-center text-center">
+            <h2 className="card-title">Vision</h2>
+            <p className="card-text">
+              To digitalize every industry left and enhance them to compete with the elite. To leave an imprint in this digital world with quality services, client testimonials, and customer satisfaction. Finally to envision ourselves as a key partner in the upcoming digital revolution for all industries and businesses.
+            </p>
           </div>
         </div>
-
-        <div className="col-md-4">
-          <div className="card hover-card" data-aos="fade-down">
-            <img src={mission} className="card-img" alt="Our Mission" />
-            <div className="card-img-overlay d-flex flex-column justify-content-center text-center">
-              <h2 className="card-title">Mission</h2>
-              <p className="card-text">
-                Our Mission is to provide next-gen solutions across all sectors and industries, creating sustainable business cultures and platforms. Fulfilling our customer ambitions is our primary motto.
-              </p>
-            </div>
+      </div>
+  
+      <div className="col-lg-4 col-md-6">
+        <div className="card hover-card" data-aos="fade-down">
+          <img src={mission} className="card-img" alt="Our Mission" />
+          <div className="card-img-overlay d-flex flex-column justify-content-center text-center">
+            <h2 className="card-title">Mission</h2>
+            <p className="card-text">
+              Our Mission is to provide next-gen solutions across all sectors and industries, creating sustainable business cultures and platforms. Fulfilling our customer ambitions is our primary motto.
+            </p>
           </div>
         </div>
-
-        <div className="col-md-4">
-          <div className="card hover-card" data-aos="fade-down">
-            <img src={corevalues} className="card-img" alt="Core Values" />
-            <div className="card-img-overlay d-flex flex-column justify-content-center text-center">
-              <h2 className="card-title">Core Values</h2>
-              <p className="card-text">
-                Client First - We work and innovate till the client is satisfied. To evolve - Never be satisfied, always get better with time. Collaborations - Value-driven and putting an end to needless processes. Frugality - A perfect blend of cost-effective creativeness.
-              </p>
-            </div>
+      </div>
+  
+      <div className="col-lg-4 col-md-12">
+        <div className="card hover-card" data-aos="fade-down">
+          <img src={corevalues} className="card-img" alt="Core Values" />
+          <div className="card-img-overlay d-flex flex-column justify-content-center text-center">
+            <h2 className="card-title">Core Values</h2>
+            <p className="card-text">
+              Client First - We work and innovate till the client is satisfied. To evolve - Never be satisfied, always get better with time. Collaborations - Value-driven and putting an end to needless processes. Frugality - A perfect blend of cost-effective creativeness.
+            </p>
           </div>
         </div>
       </div>
     </div>
+  </div>
+  
   );
 };
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -7,52 +7,40 @@ import './Header.css'
 const Header = () => {
   return (
     <> 
-   <nav className="navbar navbar-expand navbar-dark" style={{ background: 'black', fontWeight: 'bold' }}>    
-   <div className="container-fluid">
-   {/* radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(216,148,233,1) 100%) */}
-        <img src={logo}  alt="Company Logo" className="logo" />
+  <nav className="navbar navbar-expand-lg navbar-dark" style={{ background: 'black', fontWeight: 'bold' }}>    
+  <div className="container-fluid">
+    <img src={logo} alt="Company Logo" className="logo" />
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item mx-3">
+          <Link className="nav-link navbar-brand" to="about" spy={true} activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>About Us</Link>
+        </li>
+        <li className="nav-item mx-1">
+          <Link className="nav-link navbar-brand" to="service" spy={true} activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>Services</Link>
+        </li>
+        <li className="nav-item mx-1">
+          <Link className="nav-link navbar-brand" to="products" spy={true} activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>Products</Link>
+        </li>
+        <li className="nav-item mx-1">
+          <Link className="nav-link navbar-brand" to="global-network" spy={true} activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>Global Network</Link>
+        </li>
+        <li className="nav-item mx-1">
+          <Link className="nav-link navbar-brand" to="sustainability" spy={true} activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>Sustainability</Link>
+        </li>
+        <li className="nav-item mx-1">
+          <Link className="nav-link navbar-brand" to="careers" spy={true} activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>Careers</Link>
+        </li>
+        <li className="nav-item mx-1">
+          <Link className="nav-link navbar-brand" to="contact" spy={true} activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>Contact Us</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-       
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav ms-auto">
-
-          {/* <li className="nav-item mx-3">
-          <Link className="navbar-brand" to="home" spy={true} activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }} >Home</Link>
-         
-          </li> */}
-           
-
-            <li className="nav-item mx-3">
-              <Link className="nav-link navbar-brand" to="about" spy={true} activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>About Us</Link>
-            </li>
-            <li className="nav-item mx-1">
-              <Link className="nav-link navbar-brand" to="service" spy={true}  activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>Services</Link>
-            </li>
-            <li className="nav-item mx-1">
-              <Link className="nav-link navbar-brand" to="products"  spy={true} activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>Products</Link>
-            </li>
-            <li className="nav-item mx-1">
- 
-              <Link className="nav-link navbar-brand" to="global-network"  spy={true}  activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>Global Network</Link>
-            </li>
-            <li className="nav-item mx-1">
-              <Link className="nav-link navbar-brand" to="sustainability" spy={true}  activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>Sustainability</Link>
-            </li>
-            <li className="nav-item mx-1">
-              <Link className="nav-link navbar-brand" to="careers" spy={true} activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>Careers</Link>
-            </li>
-            <li className="nav-item mx-1">
-              <Link className="nav-link navbar-brand" to="contact" spy={true} activeClass="active" offset={-70} smooth={true} duration={500} style={{ cursor: 'pointer' }}>Contact Us</Link>
-            </li>
-            </ul>
-          
-        </div>
-      </div>
-    </nav>
     </>
   );
 };
